3ca383f7a1ce602d76c76e3837a4c1ed2415d89a
Michael Manville <monstro33@yahoo.com>
2019-03-25 08:12:14 -0400

    Fixed Tests

--- MyMusicCollection.Tests/HomeControllerTests .cs
+++ MyMusicCollection.Tests/HomeControllerTests .cs
@@ -1,21 +1,22 @@
-﻿//using MyMusicCollection.Controllers;
-//using MyMusicCollection.Repositories;
-//using System.Collections.Generic;
-//using Xunit;
-//using MyMusicCollection.Models;
-//using Microsoft.AspNetCore.Mvc;
+﻿using MyMusicCollection.Controllers;
+using MyMusicCollection.Repositories;
+using System.Collections.Generic;
+using Xunit;
+using MyMusicCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
 
-//namespace MyMusicCollection.Tests
-//{
-//    public class HomeControllerTests
-//    {
-//        [Fact]
-//        public void Index_Returns_A_View()
-//        {
-//            var underTest = new HomeController();
-//            var result = underTest.Index();
+namespace MyMusicCollection.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new HomeController();
+            var result = underTest.Index();
 
-//            Assert.IsType<ViewResult>(result);
-//        }
-//    }
-//}
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+    }
--- MyMusicCollection.Tests/SongRepositoryTests.cs
+++ MyMusicCollection.Tests/SongRepositoryTests.cs
@@ -1,35 +1,35 @@
-﻿//using MyMusicCollection.Models;
-//using MyMusicCollection.Repositories;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using Xunit;
+﻿using MyMusicCollection.Models;
+using MyMusicCollection.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
 
-//namespace MyMusicCollection.Tests
-//{
-//    public class SongRepositoryTests
-//    {
-//        [Fact]
-//        public void Count_Starts_At_Zero()
-//        {
-//            var db = new MusicContext();
-//            var underTest = new SongRepository(db);
+namespace MyMusicCollection.Tests
+{
+    public class SongRepositoryTests
+    {
+        [Fact]
+        public void Count_Starts_At_Zero()
+        {
+            var db = new MusicContext();
+            var underTest = new SongRepository(db);
 
-//            var count = underTest.Count();
+            var count = underTest.Count();
 
-//            Assert.Equal(0, count);
-//        }
+            Assert.Equal(0, count);
+        }
 
-//        [Fact]
-//        public void Create_Increases_Count()
-//        {
-//            var db = new MusicContext();
-//            var underTest = new SongRepository(db);
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            var db = new MusicContext();
+            var underTest = new SongRepository(db);
 
-//            underTest.Create(new Song() { Title = "Foo" });
+            underTest.Create(new Song() { SongName = "Foo" });
 
-//            var count = underTest.Count();
-//            Assert.Equal(1, count);
-//        }
-//    }
-//}
+            var count = underTest.Count();
+            Assert.Equal(1, count);
+        }
+    }
+}
